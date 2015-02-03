f3907be79fd73fb2ebf4b84fabbfedae83109097
Lennon Jesus <lennon.jesus@gmail.com>
2015-02-03 00:46:58 -0200

    adicionando angular resource

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var app = require('./config/express')();
 
 http.createServer(app).listen(app.get('port'), function () {
-	console.log('Server running on ' + app.get('port'));
+	console.log('Server running on port' + app.get('port'));
+	console.log('How to Mean is up!')
 })
 
